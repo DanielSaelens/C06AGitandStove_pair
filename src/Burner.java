26ba8672a3c2fda39b7d7226af95455c9784940d
highSpeedcoolGuy <jabuan@mines.edu>
2025-01-27 21:57:46 -0700

    Added Burner and Settings classes in java file format

---
+++ src/Burner.java
@@ -0,0 +1,22 @@
+
+public class Burner {
+
+	private final static int TIME_DURATION = 2;
+
+	private int timer;
+	private Settings mySetting;
+	
+	private Temperature myTemperature;
+	
+	Temperature getTemperature() {
+		return myTemperature;
+	}
+	
+	public enum Temperature{BLAZING, HOT, COLD, WARM};
+	
+	
+	
+	
+	
+	
+}
---
+++ src/Settings.java
@@ -0,0 +1,23 @@
+
+
+public enum Settings {
+	
+	OFF ("---"), LOW ("--+"), MEDIUM("-++"), HIGH ("+++");
+	
+	private String value;
+	
+	Settings(String value){
+		this.value = value;
+	}
+	
+	public String toString(){
+		return value;
+		
+	}
+
+	
+	
+
+
+
+}
